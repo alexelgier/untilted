15b04056f01889bdb790ac4fb885aa847e8263aa
alexelgier <alexander.elgier@gmail.com>
2016-08-03 00:57:17 -0300

    First Commit

---
+++ Futbol/Brr prueba/src/futbol/FutbolMain.java
@@ -0,0 +1,19 @@
+package futbol;
+
+public class FutbolMain {
+
+  public FutbolMain() {
+
+  }
+
+  public static void main(String[] args) {
+
+    Jugador jugador1 = new Jugador();
+    jugador1.name = "Bruno Fumagalli";
+    
+    Jugador jugador2 = new Jugador();
+    jugador2.name = "Alex Elgier";
+  
+  }
+
+}
